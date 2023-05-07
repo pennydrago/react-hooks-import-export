e14533ff2aafabcab865889db8dcd0a21bb07ded
Penelope Drago <prd@drago.com.au>
2023-05-07 21:47:34 +1000

    import variables and export component

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+// import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife} from "./parks/RockyMountain";
+
+console.log(trees);
+
+wildlife();
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  //howManyParks(); // => "42 parks!"
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  )
 
-  return <h1>Colorado State Parks!</h1>;
+  //return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
